1680a2d8f4975441764cc7cd79f4ac762f6112f4
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-15 20:43:22 -0300

    refactor: Separa e segrega as responsabilidade para Service, Model e Repository

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/missoes")
+public class MissoesController {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa é a minha primeira mensagem nessa rota!";
+    }
+}
