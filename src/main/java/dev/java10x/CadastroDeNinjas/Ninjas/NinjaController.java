ecc35e062b3159f5bceefe7c9268a83a93a92e4b
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-16 17:02:23 -0300

    feat: Adiciona novas rotas com mock de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota!";
+    }
+
+    // Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado com sucesso!";
+    }
+
+    // Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas(){
+        return "Mostrar Ninja!";
+    }
+
+    // Mostrar ninja por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorID(){
+        return "Mostrar ninja por ID!";
+    }
+
+    // Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarNinjaPorID(){
+        return "Alterar ninja por ID!";
+    }
+
+    // Deletar ninja (DELETE)
+    @DeleteMapping("deletarID")
+    public String deletarNinjaPorID(){
+        return "Ninja deletado por ID";
     }
 
 }
