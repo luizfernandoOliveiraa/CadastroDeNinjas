bbffbb7c0b5b7f45763775c38ba87efae2c0beed
luizfernando <lf.nandooliveeira@gmail.com>
2026-07-16 18:26:04 -0300

    feat: Cria service para missoes, adicionando metodo para listar todas as missoes. Adiciona no repository os endpoints para listar missoes e injeta dependencia do service no controller

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,13 +2,21 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
 
-    @GetMapping("/listar")
-    public String listarMissoes(){
-        return "Missoes listadas com sucesso";
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    @GetMapping("/listarTodasMissoes")
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarTodasMissoes();
     }
 
     // Adicionar novas missoes
